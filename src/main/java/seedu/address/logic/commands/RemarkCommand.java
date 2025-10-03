0bc92ee52497e9af5fcf420571abad483b227fbc
Chong Rui <chongrui2003@gmail.com>
2025-10-03 21:59:53 +0800

    Enhance RemarkCommand with improved success messages
    - Add MESSAGE_ADD_REMARK_SUCCESS and MESSAGE_DELETE_REMARK_SUCCESS constants
    - Implement generateSuccessMessage() method to provide contextual feedback
    - Differentiate between adding and removing remarks in user feedback
    - Use Messages.format() for consistent person formatting
    - Improve user experience with clearer command execution feedback

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -27,6 +27,8 @@ public class RemarkCommand extends Command {
             + PREFIX_REMARK + "Likes to swim.";
 
     public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
+    public static final String MESSAGE_ADD_REMARK_SUCCESS = "Added remark to Person: %1$s";
+    public static final String MESSAGE_DELETE_REMARK_SUCCESS = "Removed remark from Person: %1$s";
 
     private final Index index;
     private final Remark remark;
@@ -62,7 +64,17 @@ public class RemarkCommand extends Command {
         model.setPerson(personToEdit, editedPerson);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
 
-        return new CommandResult(String.format("Remark added to %s", editedPerson.getName()));
+        return new CommandResult(generateSuccessMessage(editedPerson));
+    }
+
+    /**
+     * Generates a command execution success message based on whether
+     * the remark is added to or removed from
+     * {@code personToEdit}.
+     */
+    private String generateSuccessMessage(Person personToEdit) {
+        String message = !remark.value.isEmpty() ? MESSAGE_ADD_REMARK_SUCCESS : MESSAGE_DELETE_REMARK_SUCCESS;
+        return String.format(message, Messages.format(personToEdit));
     }
 
     @Override
